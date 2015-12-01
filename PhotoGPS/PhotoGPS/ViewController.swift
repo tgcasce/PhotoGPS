868080f66bad388be75efda546d76cfc6205874d
Maulyn Tang <tgcasce65@163.com>
2015-12-01 17:16:45 +0800

    [add] init project.

---
+++ PhotoGPS/PhotoGPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoGPS
+//
+//  Created by DonMaulyn on 15/12/1.
+//  Copyright © 2015年 DonMaulyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
