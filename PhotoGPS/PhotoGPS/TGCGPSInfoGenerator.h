6b28d46052ac3d8cebad59d481c75723393d980c
Maulyn Tang <tgcasce65@163.com>
2015-12-04 12:09:29 +0800

    use Photos and ImageIO

---
+++ PhotoGPS/PhotoGPS/TGCGPSInfoGenerator.h
@@ -0,0 +1,13 @@
+//
+//  TGCGPSInfoGenerator.h
+//  PhotoGPS
+//
+//  Created by DonMaulyn on 15/12/1.
+//  Copyright © 2015年 DonMaulyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGCGPSInfoGenerator : NSObject
+
+@end
---
+++ PhotoGPS/PhotoGPS/TGCGPSInfoGenerator.m
@@ -0,0 +1,13 @@
+//
+//  TGCGPSInfoGenerator.m
+//  PhotoGPS
+//
+//  Created by DonMaulyn on 15/12/1.
+//  Copyright © 2015年 DonMaulyn. All rights reserved.
+//
+
+#import "TGCGPSInfoGenerator.h"
+
+@implementation TGCGPSInfoGenerator
+
+@end
